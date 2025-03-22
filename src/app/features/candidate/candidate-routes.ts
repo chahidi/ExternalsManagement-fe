00a3d64f3aeeaed13b347b9e11797704134f5700
Mohamed El Bakkali <mohamedelbakkali590@gmail.com>
2025-03-22 23:57:32 +0000

    last updates

--- src/app/features/candidate/candidate-routes.ts
+++ src/app/features/candidate/candidate-routes.ts
@@ -1,23 +1,21 @@
 import { Routes } from '@angular/router';
-import { CandidateFormComponent } from './components/candidate-form/candidate-form.component';
 
 export const CANDIDATE_FORMS_ROUTES: Routes = [
-  {
-    path: '',
-    component: CandidateFormComponent,
-    children: [
-      {
-        path: 'new-candidate',
-        loadComponent: () => import('./components/candidate-form/candidate-form.component')
-          .then(m => m.CandidateFormComponent),
-        title: 'New Candidate'
-      }
-    ],
-  },
-  {
-    path: 'new-cv',
-    loadComponent: () => import('./components/new-cv/new-cv.component')
-      .then(m => m.NewCvComponent),
-    title: 'New CV'
-  }
+    {
+        path: 'stepper',
+        loadComponent: () => import('./components/stepper-form/stepper-form.component')
+            .then(m => m.StepperFormComponent),
+        title: 'Candidate Stepper'
+    },
+    {
+        path: 'new-cv',
+        loadComponent: () => import('./components/new-cv/new-cv.component')
+            .then(m => m.NewCvComponent),
+        title: 'New CV'
+    },
+    {
+        path: '',
+        redirectTo: 'stepper',
+        pathMatch: 'full'
+    }
 ];

05a9a9319d8729c325fad86749ae6d9a84e920f8
Aicha <zouhairaisha@gmail.com>
2025-04-25 12:18:34 +0100

    i18n-multilanguage-task

--- src/app/shared/layout/components/topbar/topbar.component.ts
+++ src/app/shared/layout/components/topbar/topbar.component.ts
@@ -1,24 +1,48 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { StyleClassModule } from 'primeng/styleclass';
 import { LayoutService } from '../../service/layout.service';
+import { FormsModule } from '@angular/forms';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
     selector: 'app-topbar',
     standalone: true,
-    imports: [RouterModule, CommonModule, StyleClassModule],
+    imports: [RouterModule, CommonModule, StyleClassModule, FormsModule],
     styleUrl: './topbar.component.scss',
     templateUrl: './topbar.component.html'
 })
 
 export class AppTopbar {
     items!: MenuItem[];
+    currentLang: string;
+    showLanguageMenu: boolean = false;
 
-    constructor(public layoutService: LayoutService) {}
+    constructor(
+        public layoutService: LayoutService,
+        private translate: TranslateService
+    ) {
+        const savedLang = localStorage.getItem('userLanguage');
+        this.currentLang = savedLang || this.translate.currentLang || 'en';
+        if (savedLang) {
+            this.translate.use(savedLang);
+        }
+    }
 
     toggleDarkMode() {
         this.layoutService.layoutConfig.update((state) => ({ ...state, darkTheme: !state.darkTheme }));
+    }
+
+    toggleLanguageMenu() {
+        this.showLanguageMenu = !this.showLanguageMenu;
+    }
+
+    changeLanguage(lang: string) {
+        this.currentLang = lang;
+        localStorage.setItem('userLanguage', lang);
+        this.translate.use(lang);
+        this.showLanguageMenu = false;
     }
 }

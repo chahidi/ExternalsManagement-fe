9fc1a11bd7c9aa2dc02489fb2d5852ab4abcffc1
Rihab-Salmi <rihabsalmi2609@gmail.com>
2025-04-28 15:52:43 +0100

    fix lazy load

--- src/app/features/interviews/interviews.routes.ts
+++ src/app/features/interviews/interviews.routes.ts
@@ -1,8 +1,15 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from '../../shared/layout/layout.component'; // important!
 import { InterviewListComponent } from './interview-list/interview-list.component';
 import { InterviewsResultComponent } from './interviews-result/interviews-result.component';
 
 export const INTERVIEWS_ROUTES: Routes = [
-  { path: 'list', component: InterviewListComponent },
-  { path: 'result', component: InterviewsResultComponent }
+  {
+    path: '',
+    component: LayoutComponent,     
+    children: [
+      { path: 'list', component: InterviewListComponent },
+      { path: 'result', component: InterviewsResultComponent }
+    ]
+  }
 ];

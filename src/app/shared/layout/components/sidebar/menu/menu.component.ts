2ba8016bed25d080cc779d87eb73dc3c39574ee0
Mohamed Taha EL YAKOUBI <medtahayakoubi@gmail.com>
2025-04-29 09:45:18 +0100

    Adjustment

--- src/app/shared/layout/components/sidebar/menu/menu.component.ts
+++ src/app/shared/layout/components/sidebar/menu/menu.component.ts
@@ -46,7 +46,7 @@ export class MenuComponent {
                 ]
             },
             {
-                label: 'Test Test',
+                label: 'Test ^cknqcpqinvpq',
                 items: [
                     { label: 'Interview List', icon: 'pi pi-fw pi-list', routerLink: ['/interviews/interview-list'] },
                     { label: 'Interviews Result', icon: 'pi pi-fw pi-chart-bar', routerLink: ['/interview/interview-result'] }

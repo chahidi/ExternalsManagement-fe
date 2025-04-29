d50cc2de19497197aa9828b65e32da822ae456a4
Mohamed Taha EL YAKOUBI <medtahayakoubi@gmail.com>
2025-04-29 12:49:24 +0100

    Correction (rounting, components name)

---
+++ src/app/features/interviews/interview-result/interview-result.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-interview-result',
+  imports: [],
+  templateUrl: './interview-result.component.html',
+  styleUrl: './interview-result.component.scss'
+})
+export class InterviewResultComponent {
+
+}

83a2c68b4c8fef4de60c1adba225261f976ad6d7
Mohamed Taha EL YAKOUBI <medtahayakoubi@gmail.com>
2025-05-04 15:07:10 +0100

    Routing correction - remove child routes -

--- src/app/features/interviews/interviews.routes.ts
+++ src/app/features/interviews/interviews.routes.ts
@@ -3,21 +3,15 @@ import { InterviewListComponent } from './interview-list/interview-list.componen
 
 export const INTERVIEWS_ROUTES: Routes = [
   {
-    path: '',
-    component: InterviewListComponent,
-    children: [
-      {
-        path: 'interview-list',
-        loadComponent: () =>
-          import('./interview-list/interview-list.component').then(m => m.InterviewListComponent),
-        title: 'List of Interviews'
-      },
-      {
-        path: 'interview-result',
-        loadComponent: () =>
-          import('./interview-result/interview-result.component').then(m => m.InterviewResultComponent),
-        title: 'Interview Results'
-      }
-    ]
+    path: 'interview-list',
+    loadComponent: () =>
+      import('./interview-list/interview-list.component').then(m => m.InterviewListComponent),
+    title: 'List of Interviews'
+  },
+  {
+    path: 'interview-result',
+    loadComponent: () =>
+      import('./interview-result/interview-result.component').then(m => m.InterviewResultComponent),
+    title: 'Interview Results'
   }
 ];

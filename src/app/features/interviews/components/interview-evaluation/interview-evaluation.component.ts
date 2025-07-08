211ed9d0f7e46ddd6bc31ca1d9db5256062a8b94
youssefbouraoui1 <bouraouiyoussef12@gmail.com>
2025-07-08 14:57:12 +0100

    added evaluation column to the interviw list component so if a candiate passed an iterview we can see the evaluation by clicking on the button viewEvaluation which will redirect us to the Evaluation page that has the score a description of the performance of the candidate the title of the offer the date of the interview and the duration

--- src/app/features/interviews/components/interview-evaluation/interview-evaluation.component.ts
+++ src/app/features/interviews/components/interview-evaluation/interview-evaluation.component.ts
@@ -1,12 +1,62 @@
 import { Component, OnInit } from '@angular/core';
+import { InterviewInstance } from '../../../../core/models/interview-instance';
+import { Evaluation } from '../../../../core/models/evaluation';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
 import { InterviewEvaluationService } from '../../../../core/services/interview-evaluation.service';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { MessageModule } from 'primeng/message';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
-  selector: 'app-interview-result',
-  imports: [],
+  selector: 'app-interview-evaluation',
+  standalone: true,
+  imports: [ProgressSpinnerModule,MessageModule,CommonModule],
+  providers: [MessageService],
   templateUrl: './interview-evaluation.component.html',
-  styleUrl: './interview-evaluation.component.scss'
+  styleUrls: ['./interview-evaluation.component.scss']
 })
-export class InterviewResultComponent{
+export class InterviewEvaluationComponent implements OnInit {
+  interview: InterviewInstance | null = null;
+  evaluation: Evaluation | null = null;
+  loading = true;
+  error = {
+    happened: false,
+    message: ""
+  }
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private evaluationService: InterviewEvaluationService,
+    private messageService: MessageService
+  ) { }
+
+  ngOnInit(): void {
+    this.interview = history.state.interview;
+    if (!this.interview) {
+      this.loading = false;
+      this.error.happened = true;
+      this.error.message = "An error occured while trying to evaluate this interview could you ty again later.";
+      return;
+    }
+
+    this.evaluationService.getInterviewEvaluation("Please evaluate this candidate", this.interview.questions).subscribe({
+      next: (data) => {
+        this.evaluation = data;
+        this.loading = false;
+      },
+      error: (err) => {
+        console.error('Failed to fetch evaluation:', err);
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Error',
+          detail: 'Failed to load evaluation data'
+        });
+        this.loading = false;
+      }
+    });
+  }
 
 }

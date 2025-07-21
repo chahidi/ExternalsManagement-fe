7eabfe47392dfa561e15e020c477d95b73fe75ad
MoulayIkram <moulay.ikram23@gmail.com>
2025-07-21 22:15:53 +0200

    creating loader component

--- src/app/shared/layout/components/loader/loader.component.ts
+++ src/app/shared/layout/components/loader/loader.component.ts
@@ -7,7 +7,7 @@ import { ProgressSpinnerModule } from 'primeng/progressspinner';
   standalone: true,
   imports: [CommonModule, ProgressSpinnerModule],
   templateUrl: './loader.component.html',
-  styleUrl: './loader.component.scss'
+  styleUrls: ['./loader.component.scss']
 })
 export class LoaderComponent {
     @Input() size: string = '50px';

fd6f53c3428440b1e1e95f4233489702bb538157
youssefbouraoui1 <bouraouiyoussef12@gmail.com>
2025-08-19 12:01:42 +0100

    added first design for the page

--- src/app/features/offer/components/offer-details/offer-details.component.ts
+++ src/app/features/offer/components/offer-details/offer-details.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OfferService } from '../../../../core/services/offer.service';
 
 @Component({
   selector: 'app-offer-details',
@@ -6,6 +7,17 @@ import { Component } from '@angular/core';
   templateUrl: './offer-details.component.html',
   styleUrl: './offer-details.component.scss'
 })
-export class OfferDetailsComponent {
+export class OfferDetailsComponent implements OnInit{
+
+  isLoading : boolean = false;
+
+  constructor (
+    private offerServ : OfferService
+  ){
+  }
+
+  ngOnInit(): void {
+      
+  }
 
 }

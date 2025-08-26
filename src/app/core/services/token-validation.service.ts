5ed6a8fc20255cfb86ce05fc870401de257f108c
Rihab-Salmi <rihabsalmi2609@gmail.com>
2025-08-26 11:26:18 +0100

    removed commented code

--- src/app/core/services/token-validation.service.ts
+++ src/app/core/services/token-validation.service.ts
@@ -1,24 +1,3 @@
-// import { Injectable } from '@angular/core';
-// import { HttpClient } from '@angular/common/http';
-// import { Observable } from 'rxjs';
-// import { environment } from '../../../environments/environment';
-
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class TokenValidationService {
-//     private apiUrl = `http://localhost:3001`;
-
-
-//   constructor(private http: HttpClient) {}
-
-//   validateToken(token: string): Observable<boolean> {
-//     return this.http.post<boolean>(`${this.apiUrl}/validateToken`, { token });
-//   }
-
-// }
-
-
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';

b0fbda63d6227c13bf184c55e5cf48a4f7e7c444
Youssef Bouraoui <bouraouiyoussef12@gmail.com>
2025-10-08 20:18:08 +0100

    removed feedback general from the payload of create Interview

--- src/app/core/models/create-interview.ts
+++ src/app/core/models/create-interview.ts
@@ -4,7 +4,6 @@ import { Evaluation } from './evaluation';
 export interface CreateInterview {
     scheduledAt: Date;
     description: string;
-    feedbackGeneral: string;
     comment: string;
     estimatedDuration: number;
     candidateId: string;

13223785fac265840707706a4293e4ed6451a24a
Ismail-Ah <ahakayismail4@gmail.com>
2025-10-22 16:57:55 +0100

    create a model for candidates who pass an interview

---
+++ src/app/core/models/passed-candidate.ts
@@ -0,0 +1,13 @@
+import { Contact } from './contact';
+import { Experience } from './experience';
+import { Skill } from './skill';
+import { Education } from './education';
+import { Address } from './address';
+import { Language } from './language';
+import { Evaluation } from './evaluation';
+
+export interface PassedCandidate {
+  id: string;
+  fullName: string;
+  evaluations: Evaluation[];
+}
